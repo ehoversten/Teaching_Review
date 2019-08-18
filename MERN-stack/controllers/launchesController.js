c26cf4d3e253f410f5d948b32ac9dc842dbd1755
Ashell Fernando <ashellz2010@gmail.com>
2019-08-18 12:20:18 -0400

    started saving launches to database

--- MERN-stack/controllers/launchesController.js
+++ MERN-stack/controllers/launchesController.js
@@ -1,11 +1,44 @@
 const axios = require('axios');
+const { launchesModel } = require('../models');
 
 let url = `https://api.spacexdata.com/v3/launches`;
 
 module.exports = {
     findAll: function(req, res) {
         axios(url)
-            .then(docs => res.json(docs.data))
+            .then(docs => {
+                let data = docs.data;
+
+                data.map(launch => {
+                    let flight_number = launch.flight_number;
+
+                    launchesModel.findOne({ flight_number }).then(exist => {
+                        if (!exist || exist.length < 0) {
+                            launchesModel
+                                .create({
+                                    flight_number: launch.flight_number,
+                                    mission_name: launch.mission_name,
+                                    mission_id: launch.mission_id,
+                                    launch_year: launch.launch_year,
+                                    launch_date_local: launch.launch_date_local,
+                                    launch_success: launch.launch_success,
+                                    rocket: launch.rocket,
+                                    tbd: launch.tbd,
+                                    upcoming: launch.upcoming,
+                                    launch_site: launch.launch_site,
+                                    details: launch.details,
+                                    launch_failure_details:
+                                        launch.launch_failure_details
+                                })
+                                .then(yeet => console.log(yeet));
+                        } else {
+                            console.log(`skipped ${launch.flight_number}`);
+                        }
+                    });
+                });
+
+                res.json(data);
+            })
             .catch(err => new Error(err));
     },
     findOne: function(req, res) {
--- MERN-stack/models/spacexModels/launchesModel.js
+++ MERN-stack/models/spacexModels/launchesModel.js
@@ -2,18 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const launchSchema = new Schema({
-    flight_number: { type: Number },
-    mission_name: { type: String },
+    flight_number: Number,
+    mission_name: String,
     mission_id: [String],
     upcoming: Boolean,
-    launch_year: String,
-    launch_date_unix: Number,
-    launch_date_utc: Date,
-    launch_date_local: Date,
-    is_tentative: Boolean,
-    tentative_max_precision: String,
     tbd: Boolean,
-    launch_window: Number,
+    launch_year: String,
     rocket: {
         rocket_id: String,
         rocket_name: String,
@@ -23,14 +17,14 @@ const launchSchema = new Schema({
                 {
                     core_serial: String,
                     flight: Number,
-                    block: { type: String, required: false },
+                    block: String,
                     gridfins: Boolean,
                     legs: Boolean,
                     reused: Boolean,
-                    land_success: { type: String, required: false },
+                    land_success: String,
                     landing_intent: Boolean,
-                    landing_type: { type: String, required: false },
-                    landing_vehicle: { type: String, required: false }
+                    landing_type: String,
+                    landing_vehicle: String
                 }
             ]
         },
@@ -39,7 +33,7 @@ const launchSchema = new Schema({
             payloads: [
                 {
                     payload_id: String,
-                    norad_id: [Number],
+                    norad_id: [String],
                     reused: Boolean,
                     customers: [String],
                     nationality: String,
@@ -51,65 +45,37 @@ const launchSchema = new Schema({
                     orbit_params: {
                         reference_system: String,
                         regime: String,
-                        longitude: { type: String, required: false },
-                        semi_major_axis_km: { type: String, required: false },
-                        eccentricity: { type: String, required: false },
+                        longitude: String,
+                        semi_major_axis_km: String,
+                        eccentricity: String,
                         periapsis_km: Number,
                         apoapsis_km: Number,
                         inclination_deg: Number,
-                        period_min: { type: String, required: false },
-                        lifespan_years: { type: String, required: false },
-                        epoch: { type: String, required: false },
-                        mean_motion: { type: String, required: false },
-                        raan: { type: String, required: false },
-                        arg_of_pericenter: { type: String, required: false },
-                        mean_anomaly: { type: String, required: false }
+                        period_min: String,
+                        lifespan_years: String,
+                        epoch: String,
+                        mean_motion: String,
+                        raan: String,
+                        arg_of_pericenter: String,
+                        mean_anomaly: String
                     }
                 }
             ]
-        },
-        fairings: {
-            reused: Boolean,
-            recovery_attempt: Boolean,
-            recovered: Boolean,
-            ship: { type: String, required: false }
         }
-    },
-    ships: [],
-    telemetry: {
-        flight_club: { type: String, required: false }
     },
     launch_site: {
         site_id: String,
         site_name: String,
         site_name_long: String
     },
+    launch_date_local: String,
     launch_success: Boolean,
     launch_failure_details: {
-        time: Number,
-        altitude: { type: String, required: false },
+        time: String,
+        altitude: String,
         reason: String
     },
-    links: {
-        mission_patch: String,
-        mission_patch_small: String,
-        reddit_campaign: { type: String, required: false },
-        reddit_launch: { type: String, required: false },
-        reddit_recovery: { type: String, required: false },
-        reddit_media: { type: String, required: false },
-        presskit: { type: String, required: false },
-        article_link: String,
-        wikipedia: String,
-        video_link: String,
-        youtube_id: String,
-        flickr_images: [String]
-    },
-    details: String,
-    static_fire_date_utc: Date,
-    static_fire_date_unix: Number,
-    timeline: {
-        webcast_liftoff: Number
-    }
+    details: String
 });
 
 const launchesModel = mongoose.model('launchesModel', launchSchema);
