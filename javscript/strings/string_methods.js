d82cf5016d47487149cd17e294bcafc4d197cfb8
ehoversten <ehoversten@gmail.com>
2019-10-09 12:23:30 -0400

    created new directory for strings, moved string_methods file, added 4 methods to strings/string_methods

---
+++ javscript/strings/string_methods.js
@@ -0,0 +1,100 @@
+
+// Some useful JavaScript String Methods
+// Remember that a String in JavaScript is an OBJECT!!!
+
+let string_1 = "Who is your daddy and what does he do?";
+let string_2 = "Hello World";
+let string_3 = "do wah diddy, diddy dum, diddy do";
+
+
+// ## .length
+//  - This method returns the length of the string
+string_1.length;  // output: 38
+
+
+// ## .charAt(INDEX)
+//  - This method returns the character at the supplied INDEX value
+string_1.charAt(4);     // output: 'i'
+
+
+// ## .indexOf()
+//  - This method returns the INDEX of the FIRST INSTANCE of the supplied string or character 
+string_1.indexOf('d');   // output: 12
+string_2.indexOf('World');  // output: 6
+
+
+// ## .lastIndexOf()
+//  - This method returns the INDEX of the LAST INSTANCE of the supplied string or character 
+string_1.lastIndexOf('d');   // output: 35
+string_3.lastIndexOf('diddy');  // output: 25
+
+
+// ## .charCodeAt()
+
+
+
+// ## .fromCharCode()
+
+
+
+// ## .concat()
+
+
+
+// ## .endsWith()
+
+
+
+// ## .startsWith()
+
+
+
+// ## .includes()
+
+
+
+// ## .match()
+
+
+
+// ## .search()
+
+
+
+// ## .replace()
+
+
+
+// ## .repeat()
+
+
+
+// ## .slice()
+
+
+
+// ## .split()
+
+
+
+// ## .substr()
+
+
+
+// ## .toUpperCase()
+
+
+
+// ## .toLowerCase()
+
+
+
+// ## .trim()
+
+
+/*
+
+    A few resources:
+    
+
+*/
