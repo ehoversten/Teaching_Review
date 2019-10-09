efb7909a210bbe947e46117c8a9ff61fa32a3726
ehoversten <ehoversten@gmail.com>
2019-10-09 11:55:38 -0400

    added plan of string methods to define and give examples

--- javscript/arrays/string_methods.js
+++ javscript/arrays/string_methods.js
@@ -2,3 +2,103 @@
 // Some useful JavaScript String Methods
 
 
+
+// ## .charAt()
+
+
+
+// ## .indexOf()
+
+
+
+// ## .lastIndexOf()
+
+
+
+// ## .charCodeAt()
+
+
+
+// ## .fromCharCode()
+
+
+
+// ## .concat()
+
+
+
+// ## .endsWith()
+
+
+
+// ## .startsWith()
+
+
+
+// ## .includes()
+
+
+
+
+
+// ## .match()
+
+
+
+
+
+// ## .search()
+
+
+
+
+
+// ## .replace()
+
+
+
+
+
+// ## .repeat()
+
+
+
+
+
+// ## .slice()
+
+
+
+
+
+// ## .split()
+
+
+
+
+
+// ## .substr()
+
+
+
+
+
+// ## .toUpperCase()
+
+
+
+
+// ## .toLowerCase()
+
+
+
+
+// ## .trim()
+
+
+/*
+
+    A few resources:
+    
+
+*/
