222ec494e6539e16c557f141e76991b64e6aea83
ehoversten <ehoversten@gmail.com>
2019-10-24 17:20:30 -0400

    added filter_array challenge to algorithms, started with pseudo-codeing

---
+++ algorithms/filter_array.js
@@ -0,0 +1,43 @@
+/*
+
+    Challenge: Write a function that takes an array of numbers and a function as parameters. The function parameter should return true if the input meets a certain condition or false otherwise. Your function should run the function parameter on every element in the array parameter and , if it returns true, add the element to a new array. Return the new array.
+
+    Example:
+
+    Input: [1,2,3]  function(num){return num === 2}
+    Output: [2]
+
+    Input: [1,2,3,4,5,6]  function(num){return num % 2 === 0}
+    Output: [2,4,6]
+
+    Input: [1,2,3,4,5,6]  function(num){return num > 3}
+    Output: [4,5,6]
+
+*/
+
+// Let's start by Pseudo Coding this!!
+
+// 1) define a function that accepts 2 parameters (arr, func)
+//      - given an input, test it against our test_condition
+//          * return TRUE;
+//          * return FALSE;
+
+function passTesting(arr, func) {
+    let new_array = [];
+
+    if(test_condition) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 2) input FUNCTION is run against every elemnt in the input ARRAY
+//      - sounds like a loop or map() function -> loop through each element
+//      - test each element against a test_condition
+//          * return TRUE -> add element to NEW_ARRAY
+//          * return FALSE -> don't add that element to NEW_ARRAY
+//      + sounds like we need a NEW_ARRAY variable (let's define this up top!)
+//      - return NEW_ARRAY 
+
+
