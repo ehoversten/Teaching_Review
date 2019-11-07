2d2fe4cc873bbe7bd5829527402d1b368602fc10
ehoversten <ehoversten@gmail.com>
2019-11-07 12:10:41 -0500

    added find_vowels challenge

--- algorithms/find_vowels.js
+++ algorithms/find_vowels.js
@@ -12,15 +12,26 @@
 function vowels(str) {
     // Let's create a counting variable
     let count = 0;
-
+    // Let's have an array of our vowel characters to compare against
+    let vowelArr = ['a', 'e', 'i', 'o', 'u'];
+    // Let's also lowercase the input string so we don't have to compare capital vowel characters also
+    let lowerStr = str.toLowerCase();
+    
     // Loop through the STRING OBJECT
+    for (let i = 0; i < lowerStr.length; i++) {
 
-        // - IF character is a vowel
-
-        // increment count var
-
-        // - ELSE do nothing
-
-    // Don't forget to return count!
-    return count;
+        // Loop through vowel array
+        for(let j = 0; j < vowelArr.length; j++) {
+            // - IF character is a vowel (Compare 'str[i]' against elements in vowelArr)
+            if(str[i] == vowelArr[j]) {
+                // increment count var
+                count++;
+            }
+            // - ELSE do nothing
+        }
+    }
+        // Don't forget to return count!
+        return count;
 }
+
+console.log(vowels("Hi There"));
