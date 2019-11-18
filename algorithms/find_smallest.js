5bacff083e6f4665349481a21d08c1d538b70a00
ehoversten <ehoversten@gmail.com>
2019-11-18 14:06:53 -0500

    added find_smallest challenge, started with pseudocode

---
+++ algorithms/find_smallest.js
@@ -0,0 +1,29 @@
+/*
+
+    Challenge: Find the Smallest Number in an Array, Create a function that takes an array of numbers and returns the smallest number in the set.
+
+    Examples:
+        findSmallestNum([34, 15, 88, 2]) // output: 2
+        findSmallestNum([34, -345, -1, 100]) // output: -345
+        findSmallestNum([-76, 1.345, 1, 0]) // output: -76
+        findSmallestNum([0.4356, 0.8795, 0.5435, -0.9999]) // output: -0.9999
+        findSmallestNum([7, 7, 7]) // output: 7
+
+*/
+
+function findSmallestNum(arr) {
+    // define a variable to hold value of current smallest value
+    
+    // Loop through the given array
+
+        // check current_smallest variable against current_value of arr[i] in loop
+
+        // is current_value less than current_smallest value?
+
+            // YES? set new current_smallest to current_value[i]
+
+            // NO? keep looping through array
+
+    // Return smallest_value found
+    return small;
+}
