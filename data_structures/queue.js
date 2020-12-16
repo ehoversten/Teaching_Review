ce4b0624e329c74f63440199d8cd899201a50f54
ehoversten <ehoversten@gmail.com>
2020-12-16 13:28:53 -0500

    added enqueue() method to queue data structure

--- data_structures/queue.js
+++ data_structures/queue.js
@@ -6,7 +6,7 @@
 
     Big-O of Queues:
         - Insertion - O(1)
-        - Removal - O(1)
+        - Removal - O(n)
         - Searching - O(n)
         - Access - O(n)
 
@@ -27,4 +27,30 @@ class Queue {
         this.last = null;
         this.size = 0;
     }
+
+    // -- Pseudo Code for ENQUEUE method -- //
+        // Function accepts a VALUE parameter
+        // Create a new NODE and pass in the VALUE argument to the new NODE
+        // If SIZE of QUEUE is 0, set the new NODE to be the FIRST and LAST properties of the QUEUE
+        // Otherwise - Set the NEXT property on the CURRENT LAST to be the new NODE, then set the LAST property of the QUEUE to be the new NODE
+
+
+    enqueue(value) {
+        let newNode = new Node(value);
+        if(!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.last;
+            temp.next = newNode;
+            this.last = newNode;
+        }
+        this.size++;
+        return this.size;
+    }
+
 }
+
+// -- Create a new QUEUE -- //
+let que = new Queue();
+console.log(que);
