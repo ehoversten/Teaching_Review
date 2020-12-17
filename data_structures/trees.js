7b9765b56ebc5d45f3ffebfd5d745d20086f101a
ehoversten <ehoversten@gmail.com>
2020-12-17 12:20:19 -0500

    added insert() method to BST tree data structure

--- data_structures/trees.js
+++ data_structures/trees.js
@@ -32,9 +32,54 @@ class Node {
     }
 }
 
+// -- Define our Tree (BST) -- //
 class BinarySearchTree {
     constructor() {
         this.root = null;
+    }
+
+    // -- Pseudo Code for INSERT method -- //
+        // Create a new NODE
+        // Start at ROOT:
+            // * If ROOT is NULL, set new NODE as the ROOT
+            // * If ROOT exists, check if the VALUE of the new NODE is GREATER than or LESS than the VALUE of the ROOT
+            // * If NODE VALUE is GREATER:
+                // - Check if there is a NODE to the RIGHT
+                    // - If yes, move to CHILD NODE and repeat
+                    // - If not, add new NODE as the RIGHT property 
+            // * If NODE VALUE is LESS:
+                // Check if there is a NODE to the LEFT
+                    // - If yes, move to CHILD NODE and repeat
+                    // - If not, add new NODE as the LEFT property 
+    insert(value) {
+        let newNode = new Node(value);
+        if(!this.root) {
+            this.root = newNode;
+            console.log("ROOT");
+            return this;
+        } 
+
+        let current = this.root;
+        while(current.value) {
+            
+            if(newNode.value > current.value) {
+                if(!current.right) {
+                    current.right = newNode;
+                    console.log("Placed RIGHT");
+                    return this;
+                } 
+                current = current.right;
+            } else {
+                if(!current.left) {
+                    current.left = newNode;
+                    console.log("Placed LEFT");
+                    return this;
+                }
+                current = current.left;
+            }
+            console.log("Compare Next");
+        }
+        // return this;
     }
 }
 
